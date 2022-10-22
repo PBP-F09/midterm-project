d2792772d73ac9190a3b310c8c7f13782f8c4b03
iqbalpa <iqbalpahlevi16@gmail.com>
2022-10-22 21:59:14 +0700

    create function for register, login, logout in views

--- login/views.py
+++ login/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def registrasi_user(request):
+    form = UserCreationForm()
+    if request.method == POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registrasi Berhasil')
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'registrasi.html', context)
+
+def login_user(request):
+    if request.method == POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('beranda')
+    context = {}
+    return render(request, 'login.html', context)
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('login')
