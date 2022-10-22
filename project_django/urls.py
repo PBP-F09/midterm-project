75e77a5b4f4ad96691800d27dae1d3093df685b6
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-10-22 21:52:00 +0700

    Modify base.html

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('beranda/', include('beranda.urls')),
+    path('qna/', include('qna.urls')),
 ]
