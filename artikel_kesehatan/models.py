3b4fbc9e46c1814c9a84fa7f8b5a71ba75a75035
iqbalpa <iqbalpahlevi16@gmail.com>
2022-10-22 19:58:26 +0700

    add model class for artikel

--- artikel_kesehatan/models.py
+++ artikel_kesehatan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Artikel(models.Model):
+    judul = models.CharField(max_length=100)
+    isi = models.TextField()
+    tanggal = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    tag = models.ManyToManyField('Tag', related_name='artikel')
+    
