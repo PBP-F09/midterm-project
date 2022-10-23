a57c6327cbf9ffc931940fd0d306f7b8c089ecc3
iqbalpa <iqbalpahlevi16@gmail.com>
2022-10-23 10:29:16 +0700

    finished the models for ROLE USER and fixed bugs in registration

--- login/models.py
+++ login/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# ROLE USER
+class RoleUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Admin(RoleUser):
+    def __init__(self, user):
+        super().__init__(user)
+        is_admin = True
+class Faskes(RoleUser):
+    def __init__(self, user):
+        super().__init__(user)
+        is_faskes = True
+class Bumil(RoleUser):
+    def __init__(self, user):
+        super().__init__(user)
+        is_bumil = True
--- login/views.py
+++ login/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from login.models import Admin, Faskes, Bumil
 
 # Create your views here.
 def registrasi_user(request):
@@ -10,9 +11,21 @@ def registrasi_user(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Registrasi Berhasil')
-            return redirect('login')
+            role_user = request.POST.get('role_user')
+            user = form.save()
+            new_user = None
+            if role_user == 'admin':
+                new_user = Admin(user=user)
+            elif role_user == 'faskes':
+                new_user = Faskes(user=user)
+            elif role_user == 'bumil':
+                new_user = Bumil(user=user)
+            # user.save()
+            print(f'=========== {role_user}')
+            print(f'=========== {new_user}')
+            print(f'=========== {user}')
+            # messages.success(request, 'Registrasi Berhasil')
+            return redirect('login:login_user')
     context = {'form': form}
     return render(request, 'registrasi.html', context)
 
@@ -27,7 +40,7 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
-@login_required(login_url='login')
+@login_required(login_url='/login')
 def logout_user(request):
     logout(request)
-    return redirect('login')
+    return redirect('login_user')
