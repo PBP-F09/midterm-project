87fc9561aaae3b071af1f987e031d4ca3626a777
iqbalpa <iqbalpahlevi16@gmail.com>
2022-10-25 11:25:06 +0700

    set jquery, urls.py, and views.py for tambah artikel

--- artikel_kesehatan/urls.py
+++ artikel_kesehatan/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from artikel_kesehatan.views import show_artikel
+from artikel_kesehatan.views import show_artikel, tambah_artikel
 
 app_name = 'artikel_kesehatan'
 
 urlpatterns = [
     path('', show_artikel, name='show_artikel'),
+    path('tambah-artikel/', tambah_artikel, name='tambah_artikel'),
 ]
--- artikel_kesehatan/views.py
+++ artikel_kesehatan/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from artikel_kesehatan.models import Artikel
 
 # Create your views here.
 def show_artikel(request):
     return render(request, 'artikel.html')
+
+def tambah_artikel(request):
+    if request.method == 'POST':
+        judul = request.POST.get('judul')
+        isi = request.POST.get('isi')
+        author = request.user
+        # print(f"====== {judul}")
+        # print(f"====== {isi}")
+        # print(f"====== {author}")
+        new_artikel = Artikel(judul=judul, isi=isi, author=author)
+        new_artikel.save()
+        return redirect('artikel_kesehatan:show_artikel')
+    return render(request, 'artikel.html')
+    
