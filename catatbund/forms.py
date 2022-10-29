0d6ae94cce929d2c0bc22277720b1cb5b72ad666
dionavarastika <diona.varastika@ui.ac.id>
2022-10-30 01:33:10 +0700

    catatbund newesttt

--- catatbund/forms.py
+++ catatbund/forms.py
@@ -18,7 +18,8 @@ class TambahCatatanForm(forms.ModelForm):
         ]   
         widgets = {
             'weight': forms.TextInput(attrs={
-                'required': True,
+                'required': True, 
+                'label':'weight',
                 'name':"weight",
                 'id':"weight",
                 'class':"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white",
@@ -26,6 +27,7 @@ class TambahCatatanForm(forms.ModelForm):
             }),
             'height': forms.TextInput(attrs={
                 'required': True,
+                'label':'height',
                 'name':"height",
                 'id':"height",
                 'placeholder':"Tinggi Badan(m)",
