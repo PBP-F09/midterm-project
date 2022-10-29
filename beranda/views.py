d62a5c9bdfa81b91a094ff8cc84fc403babc6cdf
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-10-30 00:17:50 +0700

    Specifying bumil only  features

--- beranda/views.py
+++ beranda/views.py
@@ -3,4 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def show_beranda(request):
-    return render(request, 'beranda.html')
+    user_type = 'non_login'
+    if request.user.is_authenticated:
+        user_type = request.user.groups.all()[0].name
+    return render(request, 'beranda.html', {'user_type':user_type})
