8e1281329caa5a5bf3318b07585be7c292d45641
shafanjw <shafa.najwa@ui.ac.id>
2022-10-30 16:49:32 +0700

    Fix tanggal formattings

--- informasi/static/js/notes_page.js
+++ informasi/static/js/notes_page.js
@@ -1,26 +1,32 @@
-const notesBox = document.getElementById('notes-box')
-const spinnerBox = document.getElementById('spinner-box')
+$(document).ready(function () {
+    console.log("shafa")
 
-const noteForm = document.getElementById('note-form')
-const lokasi = document.getElementById('id_lokasi')
-const tanggal = document.getElementById('id_tanggal')
-const waktu = document.getElementById('id_waktu')
-const kapasitas_balita = document.getElementById('id_kapasitas_balita')
-const csrf = document.getElementsByName('csrfmiddlewaretoken')
-const alertBox = document.getElementById('alert-box')
+    updateTable();
+    
+    $('#add_info').click(function(){
+        var lokasi = $("#id_lokasi").val();
+        var tanggal = $("#id_tanggal").val();
+        var waktu = $("#id_waktu").val();
+        var kapasitas_balita = $("#id_kapasitas_balita").val();
 
-const addButton = document.getElementById('add_info')
-
-
-
-
+        $.post('/periksa/ajax/submit/',{
+            lokasi,
+            tanggal,
+            waktu,
+            kapasitas_balita,
+        },
+        function(data, status){
+            alert('Sukses menambahkan informasi!');
+            updateTable();
+            $("#id_lokasi").val("");
+            $("#id_tanggal").val("");
+            $("#id_waktu").val("");
+            $("#id_kapasitas_balita").val("");
+        });
+    })
+})
 
 function updateTable(){
-    // clear table
-    var itemCount = $(".table > tbody").children().length;
-    for(var i = itemCount; i >= 2; i--){ // remove data dari 2, karena 1 uuntuk header
-        $(".table > tbody > tr:nth-child(" + i + ")").remove();
-    }
     // fill table
     $.get('/periksa/json/', function(data) {
         for(var i = 0; i < data.length; i++){
@@ -31,29 +37,39 @@ function updateTable(){
         var cellLokasi = document.createElement("td");
         var cellTanggal = document.createElement("td");
         var cellWaktu = document.createElement("td");
-        var cellKapasitasBalita = document.createElement("td"); 
+        var cellKapasitasBalita = document.createElement("td");
+        
         var cellDelete = document.createElement("td");
+        var cellEdit = document.createElement("td");
+        
+        // var hiddenInput = document.createElement("input");
+        // hiddenInput.setAttribute("type", "hidden");
+        // hiddenInput.setAttribute("class", `buat-get-${id}`);
+        // hiddenInput.setAttribute("value", data[i].pk);
 
         var buttonDelete = document.createElement("button");
         var linkDelete = document.createElement('a');
         linkDelete.append(buttonDelete);
         linkDelete.setAttribute("href", `/periksa/delete/${data[i].pk}`)
-        buttonDelete.innerText = "Delete             ";
+        linkDelete.setAttribute("class", `bg-merah-tua w-fit text-white p-1 font-bold rounded-lg flex items-center justify-center`)
+        buttonDelete.innerText = "Delete";
 
         var buttonEdit = document.createElement("button");
-        var linkEdit = document.createElement('b');
+        var linkEdit = document.createElement('a');
+
         linkEdit.append(buttonEdit);
+        // linkEdit.setAttribute("href", `/periksa/edit/${data[i].pk}`)
         buttonEdit.setAttribute(`data-bs-toggle`, "modal")
         buttonEdit.setAttribute(`data-bs-target`, "#editModal")
         buttonEdit.innerText = "Edit";
 
         // append data ke cell
         cellLokasi.append(data[i].fields.lokasi);
-        cellTanggal.append(data[i].fields.tanggal);
+        cellTanggal.append(data[i].fields.tanggal.split("T")[0]);
         cellWaktu.append(data[i].fields.waktu);
         cellKapasitasBalita.append(data[i].fields.kapasitas_balita);
         cellDelete.append(linkDelete);
-        cellDelete.append(linkEdit);
+        cellEdit.append(linkEdit);
         
         // append cell ke row
         tr.append(cellLokasi);
@@ -61,37 +77,11 @@ function updateTable(){
         tr.append(cellWaktu);
         tr.append(cellKapasitasBalita);
         tr.append(cellDelete);
+        tr.append(cellEdit);  
+        // tr.append(hiddenInput);
         
         // add ke tabel
         $(".table").append(tr);
         }
     });
 }
-
-$(document).ready(function(){
-  updateTable()
-  
-  $('#add_info').click(function(){
-      alert('clicked');
-      var lokasi = $("#id_lokasi").val();
-      var tanggal = $("#id_tanggal").val();
-      var waktu = $("#id_waktu").val();
-      var kapasitas_balita = $("#id_kapasitas_balita").val();
-
-      $.post('/periksa/ajax/submit/',{
-          lokasi,
-          tanggal,
-          waktu,
-          kapasitas_balita,
-      },
-      function(data, status){
-          console.log("add success");
-          updateTable();
-          $("#id_lokasi").val("");
-          $("#id_tanggal").val("");
-          $("#id_waktu").val("");
-          $("#id_kapasitas_balita").val("");
-      });
-  })
-})
-
