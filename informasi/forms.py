f74cbe658039a1defa38e623d18dddd2d008d803
shafanjw <shafa.najwa@ui.ac.id>
2022-10-30 16:58:19 +0700

    Fixed PeriksaBund formatting

--- informasi/forms.py
+++ informasi/forms.py
@@ -2,12 +2,11 @@ from .models import Note
 from django import forms
 from django.forms.widgets import DateInput
 
-
 class NoteForm(forms.ModelForm):
     lokasi = forms.CharField(help_text = "Enter your Name", widget=forms.TextInput())
     tanggal= forms.DateField(initial="2022-11-30", widget=DateInput(attrs={'type': 'date'}))
     waktu = forms.CharField(widget=forms.TextInput())
-    kapasitas_balita = forms.IntegerField(widget=forms.NumberInput())
+    kapasitas_balita = forms.IntegerField(widget=forms.NumberInput(), min_value = 0)
 
     class Meta:
         model = Note
