0866a96e33755f4bdd0dcce57bfd99c04cf1e015
shafanjw <shafa.najwa@ui.ac.id>
2022-11-02 22:47:11 +0700

    Fix

--- informasi/tests/test_views.py
+++ informasi/tests/test_views.py
@@ -6,14 +6,20 @@ class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.informasi_html_url = reverse('informasi:index')
-        self.informasi_json_url = reverse('informasi:show_json')
+        self.informasi_html_url = reverse('informasi:viewInformasi')
+        self.informasi_htmlFaskes_url = reverse('informasi:show_json')
+        self.informasi_json_url = reverse('informasi:index')
 
-    def test_show_informasi_html_resolves(self):
+    def test_view_informasi_html_resolves(self):
         response = self.client.get(self.informasi_html_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'view_informasi.html')
 
     def test_show_informasi_json_resolves(self):
         response = self.client.get(self.informasi_json_url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_faskes_informasi_html_resolves(self):
+        response = self.client.get(self.informasi_htmlFaskes_url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notes_page.html')
