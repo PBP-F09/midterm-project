60fcb2f3d114f59f4ee1c9608f41e914eb228365
shafanjw <shafa.najwa@ui.ac.id>
2022-11-02 23:18:00 +0700

    Add fixed test.py

--- informasi/tests/test_views.py
+++ informasi/tests/test_views.py
@@ -7,8 +7,12 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.informasi_html_url = reverse('informasi:viewInformasi')
-        self.informasi_htmlFaskes_url = reverse('informasi:show_json')
-        self.informasi_json_url = reverse('informasi:index')
+        self.informasi_htmlFaskes_url = reverse('informasi:index')
+        self.informasi_json_url = reverse('informasi:show_json')
+
+        self.informasi_edit_url = reverse('informasi:editInfos', args=[1])
+
+ 
 
     def test_view_informasi_html_resolves(self):
         response = self.client.get(self.informasi_html_url)
@@ -17,9 +21,18 @@ class TestViews(TestCase):
 
     def test_show_informasi_json_resolves(self):
         response = self.client.get(self.informasi_json_url)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
+        # JSON always redirected, not rendered to a template
 
     def test_faskes_informasi_html_resolves(self):
         response = self.client.get(self.informasi_htmlFaskes_url)
+        self.assertEqual(response.status_code, 302)
+        # not rendered to a template
+
+    def test_edit_informasi_html_resolves(self):
+        response = self.client.get(self.informasi_edit_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'notes_page.html')
+ 
+
+    
+    
