1099fd2b42c21cb5096b8706d474dee22fbbbbf6
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-12-07 01:37:23 +0700

    Add flutter auth views

--- login/urls.py
+++ login/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/', registrasi_user, name='registrasi_user'),
     path('login/', login_user, name='login_user'),
     path('logout/', logout_user, name='logout_user'),
+    path('login-flutter/', login_flutter, name='login_flutter'),
+    path('signup-flutter/', register_flutter, name='register_flutter'),
 ]
--- login/views.py
+++ login/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from beranda.views import show_beranda
+from django.views.decorators.csrf import csrf_exempt
 
 # session and cookies
 import datetime
@@ -35,7 +36,7 @@ def login_user(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             response = HttpResponseRedirect(reverse('beranda:show_beranda'))
             response.set_cookie('last_login', str(datetime.datetime.now()))
 
@@ -55,3 +56,67 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('beranda:show_beranda'))
     response.delete_cookie('last_login')
     return response
+
+@csrf_exempt
+def login_flutter(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            # Redirect to a success page.
+            try:
+                user_type = request.user.groups.all()[0].name
+            except:
+                user_type = 'non_logged_in'
+            return JsonResponse({
+            "status": True,
+            "message": "Successfully Logged In!",
+            # Insert any extra data if you want to pass data to Flutter
+            "data":{
+                "username":username,
+                "role_user":user_type,
+            }}, status=200)
+        else:
+            return JsonResponse({
+            "status": False,
+            "message": "Failed to Login, Account Disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+        "status": False,
+        "message": "Failed to Login, check your email/password."
+        }, status=401)
+
+@csrf_exempt
+def register_flutter(request):
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password1")
+        confirm_password = request.POST.get("password2")
+        role_user = request.POST.get("role_user")
+        if (User.objects.filter(username=username)):
+            return JsonResponse({
+                "status": False,
+                "message": "Username atau email sudah pernah didaftarkan!"
+                }, status=401)
+        elif (password == confirm_password):
+            user = User.objects.create_user(username=username, password=password)
+            try:
+                group = Group.objects.get(name=role_user)
+            except:
+                group = Group.objects.create(name=role_user)
+            user.groups.add(group)
+            user.save()
+            return JsonResponse({
+                "status": True,
+                "message": "Successfully create account!"
+                # Insert any extra data if you want to pass data to Flutter
+                }, status=200)
+        else:
+            return JsonResponse({
+            "status": False,
+            "message": "Password dan Confirm Password tidak sama!"
+            }, status=401)
