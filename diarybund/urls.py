a39e8bdc78ac8cdfc65b64f2e279f4cb53a5d6a8
Mayfa Shadrina Siddi <mayfa.shadrina@ui.ac.id>
2022-12-10 19:54:44 +0700

    new views for flutter

--- diarybund/urls.py
+++ diarybund/urls.py
@@ -5,6 +5,8 @@ from diarybund.views import create_diary_ajax
 from diarybund.views import delete_ajax
 from diarybund.views import edit_diary_ajax
 from diarybund.views import show_json_flutter
+from diarybund.views import delete_ajax_flutter
+from diarybund.views import create_diary_ajax_flutter
 
 app_name = 'diarybund'
 
@@ -15,4 +17,6 @@ urlpatterns = [
     path('create-ajax/', create_diary_ajax, name='create_task_ajax'),
     path('edit/<int:id>', edit_diary_ajax, name='edit_diary_ajax'),
     path('json-flutter/', show_json_flutter, name='show_json_flutter'),
+    path('delete-flutter/<int:id>', delete_ajax_flutter, name='delete_ajax_flutter'),
+    path('create-ajax-flutter/', create_diary_ajax_flutter, name='create_diary_ajax_flutter'),
 ]
