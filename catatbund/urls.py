eebfc34cf76a6df37a1ab1bf6988555df0f936d6
dionavaras <112402619+dionavaras@users.noreply.github.com>
2022-12-12 13:53:16 +0700

    Varas/diary (#46)
    * catatbund done 2
    
    * button pull
    
    * button pink
    
    * fix edit text
    
    * test
    
    * add post get function

--- catatbund/urls.py
+++ catatbund/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('add/', add_catatan, name='add_catatan'),
     path('edit/<int:id>', edit_catatan, name='edit_catatan'),
+    path('json/<str:current_username>', show_json_flutter, name='show_json_flutter'),
+    path('add_request_flutter/', add_request_flutter, name='add_request_flutter'),
     path('delete/<int:id>/', delete_catatans, name='delete_catatan'),
 ]
